fb9f857d633cfd68c9676e89ce173616bc72482f
parkhj2009 <eee0110010100@gmail.com>
2026-01-14 23:26:54 +0900

    design: 로고 이미지를 고정 dp로 변경

--- app/src/main/java/com/linkup/android/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/linkup/android/feature/auth/signin/SignInScreen.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -49,8 +49,7 @@ fun SignInScreen(navController: NavController) {
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "Logo",
                 modifier = Modifier
-                    .padding(horizontal = 154.5.dp)
-                    .fillMaxWidth()
+                    .width(84.dp)
             )
             Text(
                 text = "로그인",
