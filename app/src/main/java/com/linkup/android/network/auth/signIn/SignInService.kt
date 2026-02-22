039f9d430259130f3b7685eba35756a16f642140
parkhj2009 <eee0110010100@gmail.com>
2026-02-22 21:40:12 +0900

    feat: Auth 서버 통신 구현

--- app/src/main/java/com/linkup/android/network/auth/signIn/SignInService.kt
+++ app/src/main/java/com/linkup/android/network/auth/signIn/SignInService.kt
@@ -2,7 +2,6 @@ package com.linkup.android.network.auth.signIn
 
 import com.linkup.android.network.BaseResponse
 import com.linkup.android.network.LinkUpUrl
-import com.linkup.android.network.auth.SignIn.SignInResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
