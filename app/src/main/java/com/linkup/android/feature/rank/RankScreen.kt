37b0c2f26e68e401648fa440811426458e8382fb
gituser <danjee2009@gmail.com>
2026-02-25 23:52:14 +0900

    column -> Lazycolumn forEach -> items

--- app/src/main/java/com/linkup/android/feature/rank/RankScreen.kt
+++ app/src/main/java/com/linkup/android/feature/rank/RankScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -24,11 +26,10 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.linkup.android.R
 import com.linkup.android.network.rank.RankResponse
 import com.linkup.android.ui.components.TopBar
@@ -157,14 +158,14 @@ fun TopCard(toplist: List<RankResponse>) {
 
 @Composable
 fun BottomCard(bottomlist: List<RankResponse>) {
-    Column(
+    LazyColumn(
         Modifier
             .padding(horizontal = 32.dp)
             .shadow(3.dp, RoundedCornerShape(12.dp))
             .background(Color.White)
             .fillMaxWidth()
     ) {
-        bottomlist.forEach { item ->
+        items(bottomlist) { item ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
